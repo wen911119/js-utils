05c77567419ee4b6e279ee62ae3dcc2dae91e97c
wen911119 <wen911119@gmail.com>
2017-12-20 23:46:22 +0800

    新增时间戳格式化

--- utils.js
+++ utils.js
@@ -7,3 +7,31 @@ export function toThousands(n) {
 }
 // toThousands(32311131.32)
 // "32,311,131.32"
+
+// 时间戳格式化
+
+export function timestampFormat(ts, template) {
+    let t = new Date(ts)
+    let year = t.getFullYear()
+    let month = _trim(t.getMonth() + 1)
+    let day = _trim(t.getDate())
+    let h = _trim(t.getHours())
+    let m = _trim(t.getMinutes())
+    let s = _trim(t.getSeconds())
+    template = template.replace('YYYY',year)
+    template = template.replace('MM',month)
+    template = template.replace('DD',day)
+    template = template.replace('hh',h)
+    template = template.replace('mm',m)
+    template = template.replace('ss',s)
+    return template
+}
+
+// 小于10的补0
+function _trim(n) {
+    n = parseInt(n)
+    if (n < 10) {
+        n = parseInt('0' + n)
+    }
+    return n
+}
